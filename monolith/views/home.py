9a50557af394f7ed79443eb921327de19a9f4a7d
Albertomac <albertomac@live.it>
2018-11-24 16:24:59 +0100

    Added _strava_auth_url implementation

--- monolith/views/home.py
+++ monolith/views/home.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from stravalib import Client
 from monolith.database import db
 from monolith.auth import current_user
 
@@ -6,16 +7,15 @@ home = Blueprint('home', __name__)
 
 
 def _strava_auth_url(config):
-    # client = Client()
-    # client_id = config['STRAVA_CLIENT_ID']
-    # redirect = 'http://127.0.0.1:5000/strava_auth'
-    # url = client.authorization_url(client_id=client_id,
-    #                                redirect_uri=redirect)
-    return ""
-
+    client    = Client()
+    client_id = config['STRAVA_CLIENT_ID']
+    redirect  = 'http://127.0.0.1:5000/strava_auth'
+    url = client.authorization_url(client_id    = client_id,
+                                   redirect_uri = redirect)
+    return url
 
 @home.route('/')
-def index():
+def index():    
     # total_average_speed = None
     # if current_user is not None and hasattr(current_user, 'id'):
     #     runs = db.session.query(Run).filter(Run.runner_id == current_user.id)
@@ -25,8 +25,9 @@ def index():
 
     # else:
     #     runs = None
-    # strava_auth_url = _strava_auth_url(home.app.config)
+    strava_auth_url = _strava_auth_url(home.app.config)
     # return render_template("index.html", runs=runs,
     #                        strava_auth_url=strava_auth_url,
     #                        total_average_speed=total_average_speed)
-    return render_template('index.html')
+    return render_template('index.html',
+                           strava_auth_url = strava_auth_url)
