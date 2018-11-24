c07c2a1af359ac825afa67182e23c140c8aa0122
Albertomac <albertomac@live.it>
2018-11-24 16:23:06 +0100

    Now strava_token is stored remotely

--- monolith/views/auth.py
+++ monolith/views/auth.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template, redirect, request
+from flask import Blueprint, render_template, redirect, request, abort, url_for
 from flask_login import (current_user, login_user, logout_user,
                          login_required)
 from stravalib import Client
 from monolith.database import db, User
 from monolith.forms import LoginForm
+from monolith.request_utils import users_endpoint, put_request_retry
+import requests
 
 
 auth = Blueprint('auth', __name__)
@@ -12,16 +14,26 @@ auth = Blueprint('auth', __name__)
 @auth.route('/strava_auth')
 @login_required
 def _strava_auth():
-    # code = request.args.get('code')
-    # client = Client()
-    # xc = client.exchange_code_for_token
-    # access_token = xc(client_id=auth.app.config['STRAVA_CLIENT_ID'],
-    #                   client_secret=auth.app.config['STRAVA_CLIENT_SECRET'],
-    #                   code=code)
-    # current_user.strava_token = access_token
-    # db.session.commit()
-    # res = fetch_runs_for_user.delay(current_user.id)
-    # res.wait()
+    code = request.args.get('code')
+    client = Client()
+    xc = client.exchange_code_for_token
+    strava_token = xc(client_id=auth.app.config['STRAVA_CLIENT_ID'],
+                      client_secret=auth.app.config['STRAVA_CLIENT_SECRET'],
+                      code=code)
+
+    try:
+        user_id = current_user.id
+        params = {'id' : user_id,
+                  'strava_token' : strava_token}
+        r = put_request_retry(users_endpoint(), user_id, body = params)
+        code = r.status_code
+        if code == 204:
+            return redirect(url_for('home.index'))
+        else:
+            return abort(400)
+    except requests.exceptions.RequestException as err:
+        print(err)
+        return abort(503)
     return redirect('/')
 
 
