25b617eb97e2a5e859e5d792ab070f87c20317e9
Albertomac <albertomac@live.it>
2018-11-24 16:24:00 +0100

    Now runs are retrived from dataservice

--- monolith/views/runs.py
+++ monolith/views/runs.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, request, abort
-from monolith.database import db, Run
+from flask import Blueprint, render_template, abort
 from monolith.auth import current_user, login_required
+from monolith.request_utils import runs_endpoint, get_request_retry
+from datetime import datetime
+import requests
 
 
 runs = Blueprint('runs', __name__)
@@ -9,12 +11,35 @@ runs = Blueprint('runs', __name__)
 @runs.route('/runs/<id>', methods=['GET'])
 @login_required
 def run(id):
-    run = db.session.query(Run).filter(Run.runner_id == current_user.id, Run.id == id).first()
-    if run is not None:
-        runId   = run.id
-        name    = run.name
-        date    = run.start_date.strftime('%A %d/%m/%y at %H:%M')
-        headers = ['Distance (m)', 'AVG Speed (m/s)', 'Elapsed Time (s)', 'Elevation (m)']
-        values  = [run.distance, run.average_speed, run.elapsed_time, run.total_elevation_gain]
-        return render_template("runs.html", name=name, date=date, headers=headers, values=values, id=runId)
-    abort(404)
+
+    try:
+        user_id = current_user.id
+        r = get_request_retry(runs_endpoint(user_id), id)
+        
+        code = r.status_code
+        if code == 200:
+            run     = r.json()
+            
+            runId   = run['id']
+            name    = run['title']
+            date    = datetime.fromtimestamp(run['start_date']).strftime('%A %d/%m/%y at %H:%M')
+            headers = ['Distance (m)',
+                       'AVG Speed (m/s)',
+                       'Elapsed Time (s)',
+                       'Elevation (m)']
+            values  = [run['distance'],
+                       run['average_speed'],
+                       run['elapsed_time'],
+                       run['total_elevation_gain']]
+            return render_template("runs.html",
+                                   name    = name,
+                                   date    = date,
+                                   headers = headers,
+                                   values  = values,
+                                   id      = runId)
+    except requests.exceptions.RequestException as err:
+        print(err)
+        return abort(503)
+
+    return abort(404)
+
