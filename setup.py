a17aac1b15a7ff0e18e8d4bba0a136d46d7e11bb
Albertomac <albertomac@live.it>
2018-11-26 18:38:50 +0100

    Moved to microservice basecode

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
+from apigateway.apigateway import __version__
 
 
-setup(name='beepbeep-monolith',
-      version="0.1",
+setup(name='beepbeep-apigateway',
+      version=__version__,
       packages=find_packages(),
       include_package_data=True,
-      zip_safe=False)
+      zip_safe=False,
+      entry_points="""
+      [console_scripts]
+      beepbeep-apigateway = apigateway.apigateway.run:main
+      """)
