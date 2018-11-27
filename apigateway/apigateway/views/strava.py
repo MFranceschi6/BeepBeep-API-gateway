423babdd645ffd38a384f75936e6a6c714f790f5
Albertomac <albertomac@live.it>
2018-11-27 18:47:03 +0100

    Added is_strava_authorized check

--- apigateway/apigateway/views/strava.py
+++ apigateway/apigateway/views/strava.py
@@ -1,16 +1,18 @@
 from flask import Blueprint, redirect
-from apigateway.auth import strava_token_required, login_required
+from apigateway.apigateway.auth import login_required
 
 strava = Blueprint('strava', __name__)
 
 
 @strava.route('/fetch')
-@strava_token_required
+# @strava_token_required
 @login_required
 def fetch():
+    # celery_app.send_task('fetch_all_runs')
+    return redirect('/')
     # res = fetch_runs_for_user.delay(current_user.id)
     # res.wait()
     # print(request.referrer)
     # if request.referrer is not None and 'login' not in request.referrer:
     #     return redirect(request.referrer)
-    return redirect('/')
+    # return redirect('/')
