8fc9b0d69f4cb7b90f3b761005a09a958af45ba7
Albertomac <albertomac@live.it>
2018-11-28 12:38:52 +0100

    Added fetch_new_runs function

--- apigateway/apigateway/views/__init__.py
+++ apigateway/apigateway/views/__init__.py
@@ -1,7 +1,7 @@
 from .home import home
 from .auth import auth as _auth
 from .users import users
-# from .strava import strava
+from .strava import strava
 from .runs import runs
 # from .training_objectives import training_objectives
 from .challenges import challenges
@@ -12,7 +12,7 @@ from .profile import profile
 blueprints = [home,
               _auth,
               users,
-              # strava,
+              strava,
               runs,
               # training_objectives,
               challenges,
--- apigateway/apigateway/views/strava.py
+++ apigateway/apigateway/views/strava.py
@@ -1,22 +1,27 @@
 import os
 from celery import Celery
 from flask import Blueprint, redirect
-from apigateway.apigateway.auth import login_required
+from apigateway.apigateway.auth import (login_required, strava_token_required,
+                                        current_user)
+
 
 strava = Blueprint('strava', __name__)
 
 
-BACKEND = BROKER = 'redis://' + os.environ[
-    'REDIS'] + ":6379" if 'REDIS' in os.environ else "redis://127.0.0.1:6379"
+BACKEND = BROKER = 'redis://' + os.environ['REDIS'] + ":6379" \
+                   if 'REDIS' in os.environ else "redis://127.0.0.1:6379"
 celery_app = Celery(__name__, backend=BACKEND, broker=BROKER)
 
 
 @strava.route('/fetch')
-# @strava_token_required
+@strava_token_required
 @login_required
-def fetch():
-    # celery_app.send_task('fetch_all_runs')
-    celery_app.send_task('datapump.datapump.periodic_fetch')
+def fetch_new_runs():
+    user_id = current_user.id
+    result = celery_app.send_task('datapump.datapump.fetch_runs_for_user',
+                                  user_id=user_id)
+
+    result.wait()
     return redirect('/')
     # res = fetch_runs_for_user.delay(current_user.id)
     # res.wait()
