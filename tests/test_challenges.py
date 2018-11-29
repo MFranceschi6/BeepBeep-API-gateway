555a7d2729292b40e9024f9ca47e8c0b1fce4183
Albertomac <albertomac@live.it>
2018-11-29 19:16:27 +0100

    Configuration for tests and mocked login_required decorator

--- tests/test_challenges.py
+++ tests/test_challenges.py
@@ -2,6 +2,17 @@ from unittest import mock
 from apigateway.apigateway.database import User
 import pytest
 import datetime
+import functools
+
+AUTH = 'apigateway.apigateway.auth'
+RUNS = 'apigateway.apigateway.views.runs'
+
+
+def login_required(func):
+    @functools.wraps(func)
+    def _login_required(*args, **kw):
+        return func(*args, **kw)
+    return _login_required
 
 def make_and_login_user(client, db_instance):
     response = client.post('/create_user', follow_redirects=True, data=dict(
@@ -25,5 +36,14 @@ def make_and_login_user(client, db_instance):
     print(response.data.decode('ascii'))
     assert b'>Hi test@email.com </' in response.data
 
-def test_one():
-    assert 2==2
+
+def test_runs(client, db_instance):
+    with mock.patch(RUNS + '.login_required', login_required):
+        with mock.patch(RUNS + '.current_user') as current_user_mock:
+            current_user_mock.id = 1
+            with mock.patch(RUNS + '.get_request_retry') as mocked:
+                obj = mocked.return_value
+                obj.status_code = 201
+
+                result = client.get('/runs/1')
+                assert result.status_code == 404
